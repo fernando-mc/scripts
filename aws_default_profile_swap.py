b83d7cde9d1b420bb916fcb02265d23a6ecb3fd3
Fernando Medina Corey <fmcorey@gmail.com>
2017-06-03 22:22:22 -0700

    Updated to use ConfigParser more elegantly
    Removes line by line edits, backup options etc. & instead relies on config parser library

--- aws_default_profile_swap.py
+++ aws_default_profile_swap.py
@@ -1,31 +1,14 @@
 #!/usr/bin/env python
 # Python Script to swap AWS Profile configs
 
-# Assumes that credentials file is at 
-# and follows this pattern:
-# [profile-name]
-# aws-key-thing: keyaskjhdaksjdhiausgd
-# aws-secret-thing: secretythingsad7uy23hjeasd8uhi2jawd
-# [profile-othername]
-# aws-key-thing: keyaskjhdaksjdhiausgd
-# aws-secret-thing: secretythingsad7uy23hjeasd8uhi2jawd
-# [default]
-# aws-key-thing: keyaskjhdaksjdhiausgd
-# aws-secret-thing: secretythingsad7uy23hjeasd8uhi2jawd
+# Assumes that credentials file is at ~/.aws/credentials
 
 import argparse
+import ConfigParser
 import os
 
-HOME_DIR = os.getenv("HOME")
-
-def read_creds_file(filename):
-    """Returns the lines of the credentials file if it exists"""
-    try:
-        with open(HOME_DIR + '/.aws/' + filename, 'r') as curr_creds:
-            curr_creds_lines = curr_creds.readlines()
-        return curr_creds_lines
-    except IOError:
-        print 'credentials file does not exist!'
+Config = ConfigParser.ConfigParser()
+Config.read('credentials')
 
 def get_profile_to_swap_to():
     """Parse the profile requested from user input"""
@@ -33,59 +16,33 @@ def get_profile_to_swap_to():
     parser.add_argument("profile", help="profile to set as default")
     args = parser.parse_args()
     profile_to_swap_to = args.profile
-    creds_string = ''
-    for line in read_creds_file('credentials'):
-        creds_string += line
-    if '[' + profile_to_swap_to + ']\n' in creds_string:
+    sections = Config.sections()
+    if profile_to_swap_to in sections:
         return profile_to_swap_to
-    elif profile_to_swap_to == 'restore':
-        return 'restore'
     else:
-        raise ValueError("The profile '{0!s}' is not in your credentials file.".format(profile_to_swap_to))
+        section_list = ''
+        for i in sections:
+            section_list += (i + '\n')
+        raise ValueError(
+            "The profile '{0}' is not in your credentials file. \n\n" \
+            "Available profiles are:\n{1}".format(
+                profile_to_swap_to, 
+                section_list
+            )
+        )
 
-def add_backup_if_none_exists():
-    """Adds a backup creds file if none exists"""
-    if read_creds_file('credentials') and not os.path.exists(HOME_DIR + '/.aws/credentials_backup'):
-        with open(HOME_DIR + '/.aws/credentials_backup', 'w') as backup_file:
-            for line in read_creds_file('credentials'):
-                backup_file.write(line)
-
-def restore_from_backup():
-    backup_file_creds_lines = read_creds_file('credentials_backup')
-    with open(HOME_DIR + '/.aws/credentials', 'w') as curr_creds:
-        for line in backup_file_creds_lines:
-            curr_creds.write(line)
-    print 'restored profile from backup'
-
-def swap_to_profile(profile):
-    curr_creds_lines = read_creds_file('credentials')
-    count = 0
-    for line in curr_creds_lines:
-        if line == '[' + profile + ']\n':
-            access_key_id = curr_creds_lines[count + 1]
-            secret_key = curr_creds_lines[count + 2]
-        count += 1
-    with open(HOME_DIR + '/.aws/credentials', 'w') as curr_creds:
-        count = 0
-        for line in curr_creds_lines:
-            if line == '[default]\n':
-                curr_creds.write('[default]\n')
-            elif curr_creds_lines[count - 1] == '[default]\n':
-                curr_creds.write(access_key_id)
-            elif curr_creds_lines[count - 2] == '[default]\n':
-                curr_creds.write(secret_key)
-            else:
-                curr_creds.write(line)
-            count += 1
-    print 'Success - Swapped profile to ' + profile
+def swap_profile(profile_to_swap_to):
+    key = Config.get(profile_to_swap_to, 'aws_access_key_id')
+    secret = Config.get(profile_to_swap_to, 'aws_secret_access_key')
+    Config.set('default', 'aws_access_key_id', key)
+    Config.set('default', 'aws_secret_access_key', secret)
+    with open('credentials', 'wb') as configfile:
+        Config.write(configfile)
 
 def main():
-    add_backup_if_none_exists()
-    profile_to_swap_to = get_profile_to_swap_to()
-    if profile_to_swap_to == 'restore':
-        restore_from_backup()
-    else: 
-        swap_to_profile(profile_to_swap_to)
-    
+    profile = get_profile_to_swap_to()
+    swap_profile(profile)
+    print 'Swapped to profile: {0}'.format(profile)
+
 if __name__ == "__main__":
     main()
